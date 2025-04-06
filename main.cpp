f071d5cd0fb492b26b4532782cc099b0a852a970
Jasurbek <j.inatullayev@newuu.uz>
2025-04-07 01:00:10 +0500

    https://github.com/akmal1216/lab-11.git

--- main.cpp
+++ main.cpp
@@ -153,3 +153,17 @@
 //     }
 //     return 0;
 // }
+
+//string problems
+
+// problem 1
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+//
+// int main() {
+//     char str[100];
+//     cin.getline(str, 100);
+//     cout << strlen(str);
+//     return 0;
+// }
